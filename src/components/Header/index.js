b45476be41ff75275f14699792e96bb5d55e4c32
Cygra <sjtuwbh@gmail.com>
2019-03-17 23:10:10 +0800

    霭芳阴未解，乍天气、过元宵。
    migrate Header style

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,31 +1,13 @@
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
+import './Header.less'
 
 const Header = ({ siteTitle }) => (
-  <div
-    className="site-header"
-    style={{
-      background: '#3f51b5',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-            fontVariant: 'small-caps',
-          }}
-        >
+  <div className="site-header">
+    <div className="site-header-content">
+      <h1>
+        <Link to="/" className="site-header-link">
           {siteTitle}
         </Link>
       </h1>
