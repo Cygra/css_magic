245c3e40874e8ac00ea757ff86134da9dc1511c5
Cygra <sjtuwbh@gmail.com>
2019-03-26 00:58:55 +0800

    自闻颖师弹，起坐在一旁。
    Filter 🚀

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,32 +1,78 @@
 import React from 'react'
-import Item from "../Item"
+import Item from '../Item'
 import './Filter.less'
 import dva from '../../images/dva.jpg'
+import { FilterItem } from './FilterItem'
 
 const divMap = [
-  'grayscale(1)',
-  'blur(2px)',
-  'sepia(0.75)',
-  'saturate(6)',
-  'hue-rotate(90deg)',
-  'invert(0.8)',
-  'opacity(0.5)',
-  'brightness(3)',
-  'contrast(4)',
+  {
+    title: 'grayscale',
+    from: 0,
+    to: 1,
+    defaultVal: 0.5,
+  },
+  {
+    title: 'blur',
+    from: 0,
+    to: 10,
+    defaultVal: 2,
+    unit: 'px',
+  },
+  {
+    title: 'sepia',
+    from: 0,
+    to: 1,
+    defaultVal: 0.8,
+  },
+  {
+    title: 'saturate',
+    from: 0,
+    to: 10,
+    defaultVal: 6,
+  },
+  {
+    title: 'hue-rotate',
+    from: 0,
+    to: 180,
+    defaultVal: 90,
+    unit: 'deg',
+  },
+  {
+    title: 'invert',
+    from: 0,
+    to: 1,
+    defaultVal: 0.8,
+  },
+  {
+    title: 'opacity',
+    from: 0,
+    to: 1,
+    defaultVal: 0.5,
+  },
+  {
+    title: 'brightness',
+    from: 0,
+    to: 10,
+    defaultVal: 3,
+  },
+  {
+    title: 'contrast',
+    from: 0,
+    to: 10,
+    defaultVal: 4,
+  },
 ]
 
 const Filter = () => (
   <Item title="Filter">
-    <div className="filter-div">
-      <img src={dva} alt="dva"/>
-      <code>no filter;</code>
-    </div>
-    {divMap.map(i => (
-      <div className="filter-div" key={i}>
-        <img src={dva} alt="dva" style={{ filter: i }} />
-        <code>filter: {i};</code>
+    Drag the slider&nbsp;<div className="filter-slider" />&nbsp;..
+    <div className="filter-item-container">
+      <div className="filter-div">
+        <img src={dva} alt="dva"/>
+        <code>no filter;</code>
       </div>
-    ))}
+      {divMap.map(i => <FilterItem {...i} key={i.title} />)}
+    </div>
   </Item>
 )
 
